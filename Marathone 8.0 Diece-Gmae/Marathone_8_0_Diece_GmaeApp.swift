bb9493954ea9d0fbf6068b84ade58a633b7f06d3
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-09-22 17:20:20 +0500

    Initial Commit

---
+++ Marathone 8.0 Diece-Gmae/Marathone_8_0_Diece_GmaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marathone_8_0_Diece_GmaeApp.swift
+//  Marathone 8.0 Diece-Gmae
+//
+//  Created by Ислам Пулатов on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Marathone_8_0_Diece_GmaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
